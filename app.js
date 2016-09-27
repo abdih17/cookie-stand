2d8189b5178ddc34a0111572bd2b81ffce2271fd
Hawa Abdi <abdih17@hotmail.com>
2016-09-26 17:08:01 -0700

    Rough Draft, worked on First and Pike

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 //First and Pike
 var firstAndPike = {
@@ -13,13 +13,14 @@ var firstAndPike = {
   totalDailySalesPerHour: 0,
   locationName: 'First and Pike',
   calcRandCustomersPerHour: function(){
-    for (var i = 0; i < hours.length - 1; i++) {
+    for (var i = 0; i < hours.length; i++) {
       this.randCustomersPerHour.push(Math.ceil(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
       console.log(this.randCustomersPerHour[i]);
     }
   },
   calcTotalCookiesPerHour: function(){
-    for (var j = 0; j < hours.length - 1; j++) {
+    this.calcRandCustomersPerHour();
+    for (var j = 0; j < hours.length; j++) {
       this.totalCookiesPerHour.push(Math.ceil(this.randCustomersPerHour[j] * this.avgCookiesPerCust));
       console.log(this.totalCookiesPerHour[j]);
       this.totalDailySalesPerHour += this.totalCookiesPerHour[j];
@@ -30,10 +31,13 @@ var firstAndPike = {
 firstAndPike.calcRandCustomersPerHour();
 firstAndPike.calcTotalCookiesPerHour();
 
-var locationName = document.getElementById('firstAndPike');
-var firstAndPikeLi = document.getElementById('li');
-firstAndPikeLi.textContent = firstAndPike.locationName;
-locationName.appendChild(firstAndPikeLi);
+for (var k = 0; k < hours.le; k++) {
+  var totalCookiesPerHour = document.getElementById('firstAndPike');
+  var firstAndPikeLi = document.createElement('li');
+  firstAndPikeLi.textContent = hours[k] + firstAndPike.totalCookiesPerHour[k];
+  console.log(totalCookiesPerHour);
+  totalCookiesPerHour.appendChild(firstAndPikeLi);
+};
 
 
 console.log(firstAndPike);
