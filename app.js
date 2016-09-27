723eb8934e63f5e5006f3fa6638c4b5f9e0d3ce8
Hawa Abdi <abdih17@hotmail.com>
2016-09-26 17:50:54 -0700

    Added on Seattle Center and Capitol Hill.

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ firstAndPikeResults();
 console.log(firstAndPike);
 
 
-
 //SeaTac Airport
 var seatacAirport = {
   minCustPerHour: 3,
@@ -92,42 +91,106 @@ function seatacAirportResults () {
 seatacAirportResults();
 
 console.log(seatacAirport);
-//
-// //Seattle Center
-// var seattleCenter = {
-//   minCustPerHour: 11,
-//   maxCustPerHour: 38,
-//   avgCookiesPerCust: 3.7,
-//   randCustomersPerHour: [],
-//   calcRandCustomersPerHour: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
-//     }
-//   }
-// };
-//
-// //Capitol Hill
-// var capitolHill= {
-//   minCustPerHour: 20,
-//   maxCustPerHour: 38,
-//   avgCookiesPerCust: 2.3,
-//   randCustomersPerHour: [],
-//   calcRandCustomersPerHour: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
-//     }
-//   }
-// };
-//
-// //Alki
-// var alki = {
-//   minCustPerHour: 2,
-//   maxCustPerHour: 16,
-//   avgCookiesPerCust: 4.6,
-//   randCustomersPerHour: [],
-//   calcRandCustomersPerHour: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
-//     }
-//   }
-// };
+
+//Seattle Center
+var seattleCenter = {
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookiesPerCust: 3.7,
+  randCustomersPerHour: [],
+  totalCookiesPerHour: [],
+  totalDailySalesPerHour: 0,
+  locationName: 'Seattle Center',
+  calcRandCustomersPerHour: function(){
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustomersPerHour.push(Math.ceil(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randCustomersPerHour[i]);
+    }
+  },
+  calcTotalCookiesPerHour: function(){
+    this.calcRandCustomersPerHour();
+    for (var j = 0; j < hours.length; j++) {
+      this.totalCookiesPerHour.push(Math.ceil(this.randCustomersPerHour[j] * this.avgCookiesPerCust));
+      console.log(this.totalCookiesPerHour[j]);
+      this.totalDailySalesPerHour += this.totalCookiesPerHour[j];
+    }
+  },
+};
+
+seattleCenter.calcRandCustomersPerHour();
+seattleCenter.calcTotalCookiesPerHour();
+
+function seattleCenterResults () {
+  for (var k = 0; k < hours.length; k++) {
+    var totalCookiesPerHour = document.getElementById('seattleCenter');
+    var seattleCenterLi = document.createElement('li');
+    seattleCenterLi.textContent = hours[k] + ': ' + seattleCenter.totalCookiesPerHour[k] + ' cookies';
+    console.log(totalCookiesPerHour);
+    totalCookiesPerHour.appendChild(seattleCenterLi);
+  };
+  seattleCenterLi.textContent = 'Total cookies for the day is ' + seattleCenter.totalDailySalesPerHour + '.';
+  totalCookiesPerHour.appendChild(seattleCenterLi);
+}
+
+seattleCenterResults();
+
+console.log(seattleCenter);
+
+
+//Capitol Hill
+var capitolHill = {
+  minCustPerHour: 20,
+  maxCustPerHour: 38,
+  avgCookiesPerCust: 2.3,
+  randCustomersPerHour: [],
+  totalCookiesPerHour: [],
+  totalDailySalesPerHour: 0,
+  locationName: 'Capitol Hill',
+  calcRandCustomersPerHour: function(){
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustomersPerHour.push(Math.ceil(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randCustomersPerHour[i]);
+    }
+  },
+  calcTotalCookiesPerHour: function(){
+    this.calcRandCustomersPerHour();
+    for (var j = 0; j < hours.length; j++) {
+      this.totalCookiesPerHour.push(Math.ceil(this.randCustomersPerHour[j] * this.avgCookiesPerCust));
+      console.log(this.totalCookiesPerHour[j]);
+      this.totalDailySalesPerHour += this.totalCookiesPerHour[j];
+    }
+  },
+};
+
+capitolHill.calcRandCustomersPerHour();
+capitolHill.calcTotalCookiesPerHour();
+
+function firstAndPikeResults () {
+  for (var k = 0; k < hours.length; k++) {
+    var totalCookiesPerHour = document.getElementById('capitolHill');
+    var capitolHillLi = document.createElement('li');
+    capitolHillLi.textContent = hours[k] + ': ' + capitolHill.totalCookiesPerHour[k] + ' cookies';
+    console.log(totalCookiesPerHour);
+    totalCookiesPerHour.appendChild(capitolHillLi);
+  };
+  capitolHillLi.textContent = 'Total cookies for the day is ' + capitolHill.totalDailySalesPerHour + '.';
+  totalCookiesPerHour.appendChild(capitolHillLi);
+}
+
+capitolHillResults();
+
+console.log(capitolHill);
+
+
+//Alki
+var alki = {
+  minCustPerHour: 2,
+  maxCustPerHour: 16,
+  avgCookiesPerCust: 4.6,
+  randCustomersPerHour: [],
+  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+    }
+  }
+};
